39a88468cf1c6b2ae9bf3642472b87937a1ba195
The Coding Canal <thecodingcanal@gmail.com>
2022-01-07 21:06:54 -0600

    got to the point where some of the website is mine

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "The Coding Canal",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "This is jesse's blog called The Coding Canal",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
